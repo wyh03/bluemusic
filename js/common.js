b3e44ab28e9486087a9a784dc9f2c949efdedf6e
wyh03 <3260368143@qq.com>
2023-08-05 20:11:03 +0800

    lanqiaomusic-native

---
+++ js/common.js
@@ -0,0 +1,14 @@
+/**
+ * 补充 searchFilter 函数
+ * 从参数 list 中获取所有 title （值如：“好不容易”） 字段包含参数 key （值如：“好”）的歌曲数据，并存入数组 result 中返回
+ * 若没有匹配数据，则返回空数组
+ * 
+ */
+const searchFilter = (key, list) => {
+  let result = []; // 用于返回搜索结果
+  // TODO：在此处补充代码实现需求
+
+  return result;
+};
+
+module.exports = searchFilter;
---
+++ js/test.js
@@ -0,0 +1,11 @@
+const searchFilter = require("./common.js");
+const songList = require("./songList.js"); //歌曲列表数据
+/**
+ * 封装单元测试函数 checkerSearchFilter 用于检测 searchFilter 函数的功能是否正确
+ * 若 searchFilter 函数未按照需求封装，则最终返回 false；
+ * 若 searchFilter 函数封装正确，则最终返回 true。
+ */
+const checkerSearchFilter = () => {
+  // TODO：按照需求补全代码
+};
+console.log(checkerSearchFilter()); // 检测需要，请勿删除
